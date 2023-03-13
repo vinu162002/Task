756b21715faa34657b52672aca6c4dc11fe3b1bd
Vinudharshini <vinu162002@gmail.com>
2023-03-13 16:46:57 +0530

    Operators

---
+++ Day15-16.02.2023-Basic Functions/assign.js
@@ -0,0 +1,29 @@
+const name='vinu';
+const h='hello '+name;
+console.log(h);
+
+if('univ'==='vinu')
+{
+console.log('Equal');
+}
+else{
+    console.log('Not same');
+}
+
+//increment decrement
+var bonusPoints=50;
+bonusPoints+=10;
+bonusPoints-=25;
+bonusPoints+=70;
+console.log(bonusPoints);
+
+//function
+let count= document.getElementById("style");
+// console.log(count);
+
+let inc = 0;
+function increase(){
+    inc= inc+1;
+    style.innerText=inc;
+    // console.log(inc);
+}
